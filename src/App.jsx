ed1fd8389c5d056afd4fa0362eb80132263bb041
Franco Saldaño <francosaldano1@gmail.com>
2023-02-10 00:50:30 -0300

    v1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from "./components/Navbar"
-import Services from "./components/Services"
+import {ServiceCarousel} from "./components/Services"
 import Presentation from './components/Presentation'
 import FAQ from './components/FAQ'
 import Footer from './components/Footer'
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="App container">
       <Navbar />
-      <main className='container flex flex-col items-center gap-10 sm:gap-28 px-20 my-20'>
+      <main className='container flex flex-col items-center gap-10 my-20 sm:gap-20 md:px-20 '>
         <Presentation />
-        <Services />
+        <ServiceCarousel/>
         <FAQ />
       </main>
       <Footer />
