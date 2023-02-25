d2342ff230edc3607c1ceb9131cda4a480eadc1e
Franco Saldaño <francosaldano1@gmail.com>
2023-02-25 01:10:57 -0300

    fixing styles not-ready

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const FQ = () => {
+const FAQ = () => {
   return (
     <div className='container'>
-      <div className='flex flex-col gap-6 divide-y grow' id="accordion-collapse" data-accordion="collapse">
+      <div className='flex flex-col gap-6 grow transition-transform' id="accordion-collapse" data-accordion="collapse">
         <h2 id="accordion-collapse-heading-1">
-          <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-x-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+          <button type="button" className="flex items-center justify-between w-full p-4 text-left text-lg rounded-sm font-UrbanistSemibold bg-palette-Peach text-gray-700 border-opacity-10 border-palette-SoftPeach focus:border-0 dark:border-gray-700 dark:text-gray-400 hover:bg-palette-SoftPeach hover:border-palette-OldRose dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
             <span>What is Flowbite?</span>
-            <svg data-accordion-icon className="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            <svg data-accordion-icon className="w-6 h-6 rotate-180 shrink-0 fill-gray-700" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
           </button>
         </h2>
         <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
-          <div className="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-            <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navBars, and more.</p>
-            <p className="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+          <div className="p-4 font-Urbanist bg-slate-100 rounded-sm border-opacity-5 border-palette-SoftPeach dark:border-gray-700 dark:bg-gray-900">
+            <p className="mb-2 font-UrbanistMedium text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navBars, and more.</p>
+            <p className="font-UrbanistMedium text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="font-UrbanistSemibold text-palette-Auburn hover:text-palette-SoftAuburn dark:text-blue-500 hover:underline hover:underline-offset-2">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
           </div>
         </div>
         <h2 id="accordion-collapse-heading-2">
@@ -52,4 +52,4 @@ const FQ = () => {
   )
 }
 
-export default FQ
+export default FAQ
