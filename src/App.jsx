9a1df23d2084f9eabaf97959acf6eb76936ad133
Franco Saldaño <francosaldano1@gmail.com>
2023-02-27 23:02:28 -0300

    irrelevant fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { ServiceCarousel } from "./components/Services";
+import { ProductCarousel } from "./components/Products";
 import Presentation from "./components/Presentation";
 import FAQ from "./components/FAQ";
 import About from "./components/About";
 import Tips from "./components/Tips";
-import Courses from "./components/Courses"
-import Products from "./components/Products"
+import Courses from "./components/Courses";
+import Services from "./components/Services";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App container">
+      <div className="App container ">
         <Navbar />
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Presentation/>}/>
-          <Route path="/QuienSoy" element={<About/>}/>
-          <Route path="/Servicios" element={<ServiceCarousel/>}/>
-          <Route path="/Productos" element={<Products/>}/>
-          <Route path="/Capacitaciones" element={<Courses/>}/>
-          <Route path="/FAQ" element={<FAQ/>}/>
-          <Route path="/Tips" element={<Tips/>}/>
+          <Route path="/quien-soy" element={<About/>}/>
+          <Route path="/servicios" element={<Services/>}/>
+          <Route path="/servicios/:servicioId" element={<Services/>}/>
+          <Route path="/productos" element={<ProductCarousel/>}/>
+          <Route path="/capacitaciones" element={<Courses/>}/>
+          <Route path="/faq" element={<FAQ/>}/>
+          <Route path="/tips" element={<Tips/>}/>
         </Routes>
         <Footer />
       </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}","./node_modules/flowbite/dist/flowbite.min.js"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/dist/flowbite.min.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        'Cremona':['Cremona','sans-serif'],
-        'Urbanist':['Urbanist','sans-serif'],
-        'UrbanistMedium':['UrbanistMedium','sans-serif'],
-        'UrbanistSemibold':['UrbanistSemibold','sans-serif'],
+        "Cremona": ["Cremona", "sans-serif"],
+        "Urbanist": ["Urbanist", "sans-serif"],
+        "UrbanistMedium": ["UrbanistMedium", "sans-serif"],
+        "UrbanistSemibold": ["UrbanistSemibold", "sans-serif"],
       },
       colors: {
         palette: {
-          Auburn: '#AA302B',
-          SoftAuburn: '#C25A5C',
-          Peach:'#F8B791',
-          SoftPeach:'#F8B8AF',
-          OldRose:'#AE7886',
-          OrchidPink: '#DEB2BF',
-          ChampagnePink:'#F1DDD6'
+          "Auburn": "#AA302B",
+          "SoftAuburn": "#C25A5C",
+          "Peach": "#F8B791",
+          "SoftPeach": "#F8B8AF",
+          "OldRose": "#AE7886",
+          "OrchidPink": "#DEB2BF",
+          "ChampagnePink": "#F1DDD6",
         },
+      },
+      gridTemplateColumns: {
+        "special": "repeat(auto-fit, minmax(300px, 1fr))",
       },
     },
   },
-  plugins: [
-    require('./node_modules/flowbite/plugin')
-  ],
+  plugins: [require("./node_modules/flowbite/plugin")],
 };
