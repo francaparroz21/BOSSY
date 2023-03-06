d6d53bcfbbbd320bbe1b535323f9e2770eefc10c
Franco Saldaño <francosaldano1@gmail.com>
2023-03-06 19:11:37 -0300

    Tips Component added

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -1,9 +1,218 @@
-import React from 'react'
+import React from "react";
 
 const Tips = () => {
+  
   return (
-    <div>Tips</div>
-  )
-}
+    <section className="container grid">
+      <span className="w-full sticky font-Urbanist text-3xl grow p-2 mb-5 indent-5 bg-palette-OrchidPink text-slate-700">
+        Tips
+      </span>
+      <div className="flex flex-col p-5 bg-palette-OrchidPink gap-16">
+        <div className="flex flex-row items-center text-left">
+          <img
+            className="border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all"
+            src="/src/assets/static/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1.png"
+            alt="belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo"
+          />
 
-export default Tips
+          <div className="relative px-5">
+            <p className="absolute -left-10 text-5xl font-UrbanistSemibold">
+              Piel Normal
+            </p>
+            <p className="mt-14 text-xl font-Urbanist">
+              ★ La piel normal debe limpiarse cuidadosamente tanto en las mañana
+              como antes de ir a dormir. Si bien utilizar productos suaves no es
+              completamente necesario, puedes usar limpiadores y desmaquillantes
+              en formato crema. <br />
+              ★ Si tienes un tipo de piel normal, es importante que uses un
+              exfoliante suave dos veces a la semana para eliminar impurezas y
+              residuos de la piel, de esta manera mantendrás tu piel suave y
+              saludable. <br />★ Utiliza jabones de fórmula suave para proteger
+              la capa superficial de la piel y no olvides usar serums y cremas
+              con ingredientes humectantes y antioxidantes, además de tu SPF
+              favorito.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-row items-center text-left">
+          <div className="relative px-5">
+            <p className="absolute -right-10 text-5xl font-UrbanistSemibold z-20">
+              Piel Normal
+            </p>
+            <p className="mt-14 text-xl font-Urbanist">
+              ★ La piel normal debe limpiarse cuidadosamente tanto en las mañana
+              como antes de ir a dormir. Si bien utilizar productos suaves no es
+              completamente necesario, puedes usar limpiadores y desmaquillantes
+              en formato crema. <br />
+              ★ Si tienes un tipo de piel normal, es importante que uses un
+              exfoliante suave dos veces a la semana para eliminar impurezas y
+              residuos de la piel, de esta manera mantendrás tu piel suave y
+              saludable. <br />★ Utiliza jabones de fórmula suave para proteger
+              la capa superficial de la piel y no olvides usar serums y cremas
+              con ingredientes humectantes y antioxidantes, además de tu SPF
+              favorito.
+            </p>
+          </div>
+          <img
+            className="border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all"
+            src="/src/assets/static/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1 (4).png"
+            alt="belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo"
+          />
+        </div>
+
+        <div className="flex flex-row items-center text-left">
+          <img
+            className="border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all"
+            src="/src/assets/static/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1 (1).png"
+            alt="belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo"
+          />
+
+          <div className="relative px-5">
+            <p className="absolute -left-10 text-5xl font-UrbanistSemibold">
+              Piel Normal
+            </p>
+            <p className="mt-14 text-xl font-Urbanist">
+              ★ La piel normal debe limpiarse cuidadosamente tanto en las mañana
+              como antes de ir a dormir. Si bien utilizar productos suaves no es
+              completamente necesario, puedes usar limpiadores y desmaquillantes
+              en formato crema. <br />
+              ★ Si tienes un tipo de piel normal, es importante que uses un
+              exfoliante suave dos veces a la semana para eliminar impurezas y
+              residuos de la piel, de esta manera mantendrás tu piel suave y
+              saludable. <br />★ Utiliza jabones de fórmula suave para proteger
+              la capa superficial de la piel y no olvides usar serums y cremas
+              con ingredientes humectantes y antioxidantes, además de tu SPF
+              favorito.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-row items-center text-left">
+          <div className="relative px-5">
+            <p className="absolute -right-10 text-5xl font-UrbanistSemibold z-20">
+              Piel Normal
+            </p>
+            <p className="mt-14 text-xl font-Urbanist">
+              ★ La piel normal debe limpiarse cuidadosamente tanto en las mañana
+              como antes de ir a dormir. Si bien utilizar productos suaves no es
+              completamente necesario, puedes usar limpiadores y desmaquillantes
+              en formato crema. <br />
+              ★ Si tienes un tipo de piel normal, es importante que uses un
+              exfoliante suave dos veces a la semana para eliminar impurezas y
+              residuos de la piel, de esta manera mantendrás tu piel suave y
+              saludable. <br />★ Utiliza jabones de fórmula suave para proteger
+              la capa superficial de la piel y no olvides usar serums y cremas
+              con ingredientes humectantes y antioxidantes, además de tu SPF
+              favorito.
+            </p>
+          </div>
+          <img
+            className="border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all"
+            src="/src/assets/static/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1 (2).png"
+            alt="belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo"
+          />
+        </div>
+
+        <div className="flex flex-row items-center text-left">
+          <img
+            className="border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all"
+            src="/src/assets/static/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1 (3).png"
+            alt="belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo"
+          />
+
+          <div className="relative px-5">
+            <p className="absolute -left-10 text-5xl font-UrbanistSemibold">
+              Piel Normal
+            </p>
+            <p className="mt-14 text-xl font-Urbanist">
+              ★ La piel normal debe limpiarse cuidadosamente tanto en las mañana
+              como antes de ir a dormir. Si bien utilizar productos suaves no es
+              completamente necesario, puedes usar limpiadores y desmaquillantes
+              en formato crema. <br />
+              ★ Si tienes un tipo de piel normal, es importante que uses un
+              exfoliante suave dos veces a la semana para eliminar impurezas y
+              residuos de la piel, de esta manera mantendrás tu piel suave y
+              saludable. <br />★ Utiliza jabones de fórmula suave para proteger
+              la capa superficial de la piel y no olvides usar serums y cremas
+              con ingredientes humectantes y antioxidantes, además de tu SPF
+              favorito.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="p-10 bg-palette-OrchidPink">
+        <div className="p-1 bg-gradient-to-b from-palette-OldRose to-palette-ChampagnePink">
+          <div className=" flex flex-col gap-2 p-5 py-10 bg-palette-OrchidPink  z-20">
+            <p className="text-5xl font-UrbanistSemibold">
+              Para tener en cuenta
+            </p>
+            <p className="text-xl font-Urbanist">
+              ★ Para obtener resultados con una nueva rutina, tienes que esperar
+              1-3 meses. <br />
+              ★ Desmaquillante antes de irte a dormir. <br />
+              ★ Conserva tus mascarillas en el refrigerador. Hidrata y refresca
+              al mismo tiempo. <br />★ Importante cuidar la piel de nuestras
+              manos. Aplicando hidratante, exfoliando semanalmente y protector
+              solar.
+            </p>
+          </div>
+        </div>
+      </div>
+      <span className="w-full grow sticky font-Cremona text-6xl text-center py-10 bg-palette-OrchidPink text-slate-700">
+        AFIRMACIONES
+      </span>
+      <div className="bg-fixed bg-origin-padding bg-center bg-no-repeat bg-cover bg-sky-img">
+        <div className="grid grid-cols-3 auto-rows-8 gap-3 px-20 my-5">
+          <div className="flex items-center row-start-1 row-end-4 py-18 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className=" text-stone-800 font-Cremona text-lg text-left px-6">
+              ESTOY ABIERTA Y RECEPTIVA A RECIBIR TODA MAGIA QUE HAY PARA MÍ
+            </p>
+          </div>
+          <div className="flex items-center row-start-1 row-end-3 py-14 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className="text-stone-800 font-Cremona text-lg text-left px-6">
+              ATRAIGO TODA LA RIQUEZA QUE NECESITO Y DESEO CON EL MISMO ESFUERZO.
+            </p>
+          </div>
+          <div className="flex items-center row-start-1 row-end-4 py-18 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className="text-stone-800 font-Cremona text-lg text-left px-6">
+              ESTOY ALINEADA CON EL UNIVERSO Y TENGO EL PODER DE VIVIR LA VIDA DE MIS SUEÑOS.
+            </p>
+          </div>
+          <div className="flex items-center row-start-4 row-end-6 py-30 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className="text-stone-800 font-Cremona text-lg text-left px-6">
+              SOY ABUNDANTE DE MENTE, CUERPO, ALMA Y BOLSILLO.
+            </p>
+          </div>
+          <div className="flex items-center row-start-3 row-end-5 py-14 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className="text-stone-800 font-Cremona text-lg text-left px-6">
+              TODO LO QUE INVIERTO EN MI, REGRESA 70 VECES 7.
+            </p>
+          </div>
+          <div className="flex items-center row-start-4 row-end-6 py-30 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className="text-stone-800 font-Cremona text-lg text-left px-6">
+              TODO LO QUE YO QUIERO SE MANIFIESTA.
+            </p>
+          </div>
+          <div className="flex items-center row-start-6 row-end-auto py-14 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className="text-stone-800 font-Cremona text-lg text-left px-6">
+              TODO LO QUE DESEO LLEGA A MI CON FLUIDEZ Y FACILIDAD.
+            </p>
+          </div>
+          <div className="flex items-center row-start-5 row-end-7 py-24 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className="text-stone-800 font-Cremona text-lg text-left px-6">
+              CREER EN TU TALENTO, EN TU POTENCIAL Y RESPETAR TU ESFUERZO ES DE LAS MEJORES FORMAS DE AMARTE.
+            </p>
+          </div>
+          <div className="flex items-center row-start-6 row-end-auto py-14 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm">
+            <p className="text-stone-800 font-Cremona text-lg text-left px-6">
+              TODO EL UNIVERSO CONSPIRA A MI FAVOR AQUÍ Y AHORA.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tips;
