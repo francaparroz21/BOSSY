e5ee1f62d9d2c19e488122be62c53d435d7279a6
Franco Saldaño <francosaldano1@gmail.com>
2023-03-22 21:17:37 -0300

    About-MOBILE

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,15 @@
 const About = () => {
   return (
-    <div className='container relative flex justify-around my-7 bg-gradient-to-b from-palette-OrchidPink to-palette-ChampagnePink '>
-      <div className='relative basis-1/3 overflow-hidden self-center h-[33rem] '>
+    <div className='relative md:my-7 flex flex-col md:gap-5 justify-around items-center  md:flex-row  bg-gradient-to-b from-palette-OrchidPink to-palette-ChampagnePink '>
+      <div className='flex relative md:basis-1/3 overflow-hidden self-center md:h-[33rem] '>
         <img
-          className='absolute w-[22rem] object-cover -top-10 left-[10%] elipseabout'
+          className='elipseAbout-small md:absolute w-[10rem] md:w-[22rem] md:object-cover md:-top-10 md:left-[10%] hover:brightness-105 transition-all'
           src='/src/assets/static/imgpage/4ED63C86-96F4-4105-9AB9-C0A86FBA9FD2.jpg'
           alt='imagen-de-valentina-bossy'
         />
       </div>
-
-      <div className='basis-2/3 self-center z-20'>
-        <p className='text-slate-700 pr-2 text-lg lg:text-2xl font-Urbanist'>
+      <div className='flex md:basis-2/3 self-center z-20'>
+        <p className=' text-base px-5 lg:pr-2 lg:text-2xl font-Urbanist text-palette-SoftAuburn'>
           Mi nombre es Valentina Bossy, creadora de BOSSY SKINCARE. Soy
           maquilladora, masajista profesional, cosmetóloga, cosmiatra holística,
           dermatocosmíatra, brow & lash artist. <br /> Mi pasión es el mundo de
@@ -25,7 +24,12 @@ const About = () => {
         </p>
       </div>
       <img
-        className='absolute right-0 top-[55%] z-0 hover:brightness-105'
+        className='absolute w-[30%] top-[10%] left-[55%] md:right-0 md:top-[55%] z-0 hover:brightness-105 transition-all'
+        src='/src/assets/static/imgpage/liliumm 1.png'
+        alt=''
+      />
+      <img
+        className='absolute w-[30%] top-[0%] left-[12%] flipY md:hidden z-0 hover:brightness-105 transition-all'
         src='/src/assets/static/imgpage/liliumm 1.png'
         alt=''
       />
