5b9ba435e04902dc030203c1df33d33800b4a168
Franco Saldaño <francosaldano1@gmail.com>
2023-03-31 00:04:56 -0300

    body to GRID fix App css

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Footer from './components/Footer'
 function App () {
   return (
     <BrowserRouter>
-      <div className='App container relative w-[97vw] overflow-y-hidden'>
+      <div className='App w-full relative '>
         <Navbar />
         <Routes>
           <Route path='/' element={<Presentation />} />
