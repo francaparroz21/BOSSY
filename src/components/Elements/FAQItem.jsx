a5be05b09617148cb74b8abbe29b702a2482f95c
Franco Saldaño <francosaldano1@gmail.com>
2023-04-10 21:37:36 -0300

    big update Faq new logic to optimize the page not use framework

--- src/components/Elements/FAQItem.jsx
+++ src/components/Elements/FAQItem.jsx
@@ -1,67 +1,53 @@
 import { useState } from 'react'
 
-const FAQItem = ({ pregunta, respuesta, index }) => {
+const FAQItem = ({ pregunta, respuesta, index, isOpen, onClick }) => {
   const [toggle, setToggle] = useState(true)
   const [hidden, setHidden] = useState('hidden')
 
-  const Toggle = () => {
-    setToggle(!toggle)
-    toggle ? setHidden('') : setHidden('hidden')
-    console.log(hidden)
-  }
-
   return (
     <>
-      <h2 id={`accordion-collapse-heading-${index + 1}`}>
-        <button
-          className='flex items-center justify-between w-full grow p-2 text-left text-xl text-stone-700 rounded-lg font-Urbanist bg-palette-SoftPeach border-opacity-40 focus:ring-palette-SoftPeach focus:outline-none focus:bg-palette-Peach dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800'
-          data-accordion-target={`#accordion-collapse-body-${index + 1}`}
-          aria-controls={`accordion-collapse-body-${index + 1}`}
-          aria-expanded='true'
-          type='button'
-          onClick={() => Toggle()}
+      <button
+        className='flex items-center justify-between w-full grow p-2 text-left text-xl text-stone-700 rounded-lg font-Urbanist bg-palette-SoftPeach border-opacity-40 focus:ring-palette-SoftPeach focus:outline-none focus:bg-palette-Peach dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800'
+        onClick={onClick}
+      >
+        <p className='text-sm md:text-base'>{pregunta}</p>
+        <svg
+          viewBox='0 0 24 24'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          data-accordion-icon
+          className={`w-6 h-6 shrink-0 fill-gray-700 transition-all ` + (isOpen ? 'rotate-45' : 'rotate-90')}
         >
-          <span>{pregunta}</span>
-          <svg
-            viewBox='0 0 24 24'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-            data-accordion-icon
-            className='w-6 h-6 rotate-180 shrink-0 fill-gray-700'
-          >
-            <g id='SVGRepo_bgCarrier' strokeWidth='0' />
-            <g
-              id='SVGRepo_tracerCarrier'
+          <g id='SVGRepo_bgCarrier' strokeWidth='0' />
+          <g
+            id='SVGRepo_tracerCarrier'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+          <g id='SVGRepo_iconCarrier'>
+            {' '}
+            <path
+              d='M5 12H19'
+              stroke='#323232'
+              strokeWidth='2'
               strokeLinecap='round'
               strokeLinejoin='round'
-            />
-            <g id='SVGRepo_iconCarrier'>
-              {' '}
-              <path
-                d='M5 12H19'
-                stroke='#323232'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              />{' '}
-              <path
-                d='M12 5L12 19'
-                stroke='#323232'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              />{' '}
-            </g>
-          </svg>
-        </button>
-      </h2>
+            />{' '}
+            <path
+              d='M12 5L12 19'
+              stroke='#323232'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />{' '}
+          </g>
+        </svg>
+      </button>
       <div
-        id={`accordion-collapse-body-${index + 1}`}
-        className={hidden}
-        aria-labelledby={`accordion-collapse-heading-${index + 1}`}
+        className={isOpen ? '' : 'hidden'}
       >
-        <div className='p-4 font-Urbanist bg-inherit rounded-sm border-opacity-5 border-palette-SoftPeach dark:border-gray-700 dark:bg-gray-900'>
-          <p className='mb-2 font-UrbanistMedium text-gray-500 dark:text-gray-400'>
+        <div className='px-2 md:p-2 mb-3 font-Urbanist bg-inherit rounded-sm border-opacity-5 border-palette-SoftPeach dark:border-gray-700 dark:bg-gray-900'>
+          <p className=' text-xs md:text-sm lg:text-base font-UrbanistMedium text-gray-500 dark:text-gray-400'>
             {respuesta}
           </p>
         </div>
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,34 +1,37 @@
+import { useState } from 'react'
 import { faq } from '../assets/courses/data.json'
 import FAQItem from './Elements/FAQItem'
+import Headers from './Elements/Headers'
 
 const Faq = () => {
+  const [activeIndex, setActiveIndex] = useState(-1)
+  const handleClick = (index) => {
+    setActiveIndex(index === activeIndex ? -1 : index)
+  }
   return (
-    <div className='container grid'>
-      <span className='w-full sticky font-Urbanist text-3xl grow p-2 indent-5 bg-palette-OrchidPink text-slate-700'>
-        Preguntas Frecuentes
-      </span>
+    <section className='w-full grid'>
+      <Headers title={'Preguntas Frecuentes'} />
       <div className='flex'>
         <div
-          className='basis-2/3 flex flex-col gap-4 grow p-5 pt-8 justify-evenly transition-transform'
-          id='accordion-collapse'
-          data-accordion='collapse'
-        >
+          className='basis-2/3 flex flex-col gap-3 grow p-6 md:p-5 justify-evenly transition-transform'>
           {faq.preguntas.map((pregunta, i) => (
             <FAQItem
-              pregunta={pregunta}
-              respuesta={faq.respuestas[i]}
               index={i}
               key={i}
+              pregunta={pregunta}
+              respuesta={faq.respuestas[i]}
+              isOpen={i === activeIndex}
+              onClick={() => handleClick(i)}
             />
           ))}
         </div>
-        <div className='basis-1/3 flex flex-col justify-center'>
+        <aside className='hidden basis-1/3 lg:flex lg:flex-col justify-center'>
           <img src='/assets/imgpage/Rectangle 64.png' alt='' />
           <img src='/assets/imgpage/Rectangle 66.png' alt='' />
           <img src='/assets/imgpage/Rectangle 67.png' alt='' />
-        </div>
+        </aside>
       </div>
-    </div>
+    </section>
   )
 }
 
