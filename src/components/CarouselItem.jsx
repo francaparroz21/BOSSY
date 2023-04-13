96055bafc2cf151eb1f5e8c9feb440778dace89e
Franco Saldaño <francosaldano1@gmail.com>
2023-04-12 23:20:32 -0300

    Carrousel items responsive

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -7,6 +7,16 @@ const CarouselItem = ({
   desc,
   img
 }) => {
+  const Text = ({ children }) =>
+    <p className='text-sm md:text-lg  text-slate-700 clear-left font-Urbanist'>
+      {children}
+    </p>
+  const SubTitle = ({ children }) =>
+    <p className='text-base md:text-lg font-semibold text-slate-700 font-UrbanistSemibold'>
+      {children}
+    </p>
+
+
   const Check = (item) => {
     return item && item.length !== 0
   }
@@ -16,75 +26,118 @@ const CarouselItem = ({
       <div className='hidden duration-150 ease-linear' data-carousel-item>
         <img
           src={`/assets/imgproducts/${img}`}
-          className='absolute block w-[25%] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/4 hover:brightness-105 transition-all'
+          className='absolute block w-[45%] sm:w-[30%] md:w-[30%] lg:w-[25%] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 lg:left-1/4 hover:brightness-105 transition-all'
           alt={`${img}`}
         />
 
-        <div className='absolute block w-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2 -right-1/4 mr-24'>
+        <div className='absolute hidden lg:block w-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2 -right-1/4 mr-24'>
           <div className='flex flex-col bg-[#F5F5F5]/70 p-3 px-5 rounded-xl'>
             {Check(title)
               ? (
-                <p className=' font-bold font-UrbanistBold mb-2'>{title}</p>
-                )
+                <p className='font-base font-bold font-UrbanistBold mb-2 text-slate-700'>{title}</p>
+              )
               : (
-                  ''
-                )}
+                <></>
+              )}
             {Check(desc)
               ? (
                 <>
-                  <p className=' text-lg font-semibold font-UrbanistSemibold'>
-                    Detalle
-                  </p>
-                  <p className=' text-slate-700 clear-left text-lg font-Urbanist'>
-                    {desc}
-                  </p>
+                  <SubTitle>{'Detalle'}</SubTitle>
+                  <Text>{desc}</Text>
                 </>
-                )
+              )
               : (
-                  ''
-                )}
+                <></>
+              )}
             {Check(benef)
               ? (
                 <>
-                  <p className=' text-lg font-semibold font-UrbanistSemibold'>
-                    Beneficios
-                  </p>
-                  <p className=' text-slate-700 clear-left text-lg font-Urbanist'>
-                    {benef}
-                  </p>
+                  <SubTitle>{'Beneficios'}</SubTitle>
+                  <Text>{benef}</Text>
                 </>
-                )
+              )
               : (
-                  ''
-                )}
+                <></>
+              )}
             {Check(principiosactivos)
               ? (
                 <>
-                  <p className=' text-lg font-UrbanistSemibold'>
-                    Principios Activos
-                  </p>
-                  <p className=' text-slate-700 clear-left text-lg font-Urbanist'>
-                    {principiosactivos}
-                  </p>{' '}
+                  <SubTitle>{'Principios activos'}</SubTitle>
+                  <Text>{principiosactivos}</Text>
                 </>
-                )
+              )
               : (
-                  ''
-                )}
+                <></>
+              )}
             {Check(mododeuso)
               ? (
                 <>
-                  <p className=' text-lg font-UrbanistSemibold'>Modo de uso</p>
-                  <p className=' text-slate-700 clear-left text-lg font-Urbanist'>
-                    {mododeuso}
-                  </p>
+                  <SubTitle>{'Modo de uso'}</SubTitle>
+                  <Text>{mododeuso}</Text>
                 </>
-                )
+              )
               : (
-                  ''
-                )}
+                <></>
+              )}
             <NavLink
               className='inline-flex items-center px-12 py-1.5 mt-auto ml-auto text-center text-lg font-UrbanistMedium text-gray-100 bg-palette-SoftAuburn rounded-lg hover:bg-palette-Auburn focus:ring-3 focus:outline-none focus:ring-palette-ChampagnePink'
+              to='#'
+            >
+              Ver Catálogo
+            </NavLink>
+          </div>
+        </div>
+        <div className='absolute block lg:hidden w-full top-full'>
+          <div className='flex flex-col bg-[#F5F5F5] p-3 sm:px-7'>
+            {Check(title)
+              ? (
+                <p className='text-xs md:text-base font-bold font-UrbanistBold mb-2 text-slate-700'>{title}</p>
+              )
+              : (
+                <></>
+              )}
+            {Check(desc)
+              ? (
+                <>
+                  <SubTitle>{'Detalle'}</SubTitle>
+                  <Text>{desc}</Text>
+                </>
+              )
+              : (
+                <></>
+              )}
+            {Check(benef)
+              ? (
+                <>
+                  <SubTitle>{'Beneficios'}</SubTitle>
+                  <Text>{benef}</Text>
+                </>
+              )
+              : (
+                <></>
+              )}
+            {Check(principiosactivos)
+              ? (
+                <>
+                  <SubTitle>{'Principios activos'}</SubTitle>
+                  <Text>{principiosactivos}</Text>
+                </>
+              )
+              : (
+                <></>
+              )}
+            {Check(mododeuso)
+              ? (
+                <>
+                  <SubTitle>{'Modo de uso'}</SubTitle>
+                  <Text>{mododeuso}</Text>
+                </>
+              )
+              : (
+                <></>
+              )}
+            <NavLink
+              className='inline-flex items-center px-12 py-1.5 mt-3 ml-auto text-center text-xs md:text-lg font-UrbanistMedium text-gray-100 bg-palette-SoftAuburn rounded-lg hover:bg-palette-Auburn focus:ring-3 focus:outline-none focus:ring-palette-ChampagnePink'
               to='#'
             >
               Ver Catálogo
--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -1,22 +1,38 @@
+import Headers from './Elements/Headers'
 const Tips = () => {
+
+  const msj = [
+    'ESTOY ABIERTA Y RECEPTIVA A RECIBIR TODA MAGIA QUE HAY PARA MÍ',
+    'ATRAIGO TODA LA RIQUEZA QUE NECESITO Y DESEO CON EL MISMO ESFUERZO.',
+    'ESTOY ALINEADA CON EL UNIVERSO Y TENGO EL PODER DE VIVIR LA VIDA DE MIS SUEÑOS.',
+    'SOY ABUNDANTE DE MENTE, CUERPO, ALMA Y BOLSILLO.',
+    'TODO LO QUE INVIERTO EN MI, REGRESA 70 VECES 7.',
+    'TODO LO QUE YO QUIERO SE MANIFIESTA.',
+    'TODO LO QUE DESEO LLEGA A MI CON FLUIDEZ Y FACILIDAD.',
+    'CREER EN TU TALENTO, EN TU POTENCIAL Y RESPETAR TU ESFUERZO ES DE LAS MEJORES FORMAS DE AMARTE.',
+    'TODO EL UNIVERSO CONSPIRA A MI FAVOR AQUÍ Y AHORA.']
+
+  const PTips = ({ type }) =>
+    <p className='px-6 text-xs lg:text-base md:text-lg text-left text-stone-800 font-Cremona'>
+      {`${msj[type]}`}
+    </p>
+
   return (
-    <section className='container grid'>
-      <span className='w-full sticky font-Urbanist text-3xl grow p-2 mb-5 indent-5 bg-palette-OrchidPink text-slate-700'>
-        Tips
-      </span>
-      <div className='flex flex-col p-5 bg-palette-OrchidPink gap-16'>
-        <div className='flex flex-row items-center text-left'>
+    <section className='w-full grid'>
+      <Headers title={'Tips'} />
+      <div className='flex flex-col mt-5 p-5 gap-8 lg:gap-16 bg-palette-OrchidPink '>
+        <article className='flex flex-col lg:flex-row items-center gap-3 text-left'>
           <img
-            className='border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
-            src='/src/assets/static/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1.png'
+            className='w-1/2 sm:w-[45%] md:w-[45%] lg:w-auto pt-0 mr-auto lg:mx-5 md:pt-2 lg:pt-10 border-[8px] md:border-[14px] lg:border-[18px] rounded-2xl aspect-auto bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
+            src='/assets/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1.png'
             alt='belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo'
           />
 
           <div className='relative px-5'>
-            <p className='absolute -left-10 text-5xl font-UrbanistSemibold'>
+            <h3 className='absolute -top-24 right-5 md:-top-44 lg:top-10 lg:-left-10 text-3xl md:text-5xl font-UrbanistSemibold'>
               Piel Normal
-            </p>
-            <p className='mt-14 text-xl font-Urbanist'>
+            </h3>
+            <p className='lg:mt-24 text-base md:text-xl lg:text-xl font-Urbanist'>
               ★ La piel normal debe limpiarse cuidadosamente tanto en las mañana
               como antes de ir a dormir. Si bien utilizar productos suaves no es
               completamente necesario, puedes usar limpiadores y desmaquillantes
@@ -30,14 +46,14 @@ const Tips = () => {
               favorito.
             </p>
           </div>
-        </div>
+        </article>
 
-        <div className='flex flex-row items-center text-left'>
+        <article className='flex flex-col-reverse lg:flex-row items-center gap-3 text-left'>
           <div className='relative px-5'>
-            <p className='absolute -right-10 text-5xl font-UrbanistSemibold z-20'>
+            <h3 className='absolute -top-24 md:-top-44 lg:top-0 lg:-right-10 text-3xl md:text-5xl font-UrbanistSemibold z-20'>
               Piel Mixta
-            </p>
-            <p className='mt-14 text-xl font-Urbanist'>
+            </h3>
+            <p className='lg:mt-14 text-base md:text-xl font-Urbanist'>
               ★ Utiliza cremas hidratantes específicas para el tipo de piel
               mixta, preferiblemente con vitamina C para prevenir la flacidez.
               Evita las cremas con base de aceite. <br />
@@ -50,24 +66,24 @@ const Tips = () => {
             </p>
           </div>
           <img
-            className='border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
+            className='w-1/2 sm:w-[45%] md:w-1/2 lg:w-auto pt-0 ml-auto lg:mx-5 md:pt-2 lg:pt-10 border-[8px] md:border-[14px] lg:border-[18px] rounded-2xl aspect-auto bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
             src='/assets/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1 (4).png'
             alt='belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo'
           />
-        </div>
+        </article>
 
-        <div className='flex flex-row items-center text-left'>
+        <article className='flex flex-col lg:flex-row items-center gap-3 text-left'>
           <img
-            className='border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
+            className='w-1/2 sm:w-[45%] md:w-1/2 lg:w-auto pt-0 mr-auto lg:mx-5 md:pt-2 lg:pt-10 border-[8px] md:border-[14px] lg:border-[18px] rounded-2xl aspect-auto bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
             src='/assets/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1 (1).png'
             alt='belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo'
           />
 
           <div className='relative px-5'>
-            <p className='absolute -left-10 text-5xl font-UrbanistSemibold'>
+            <h3 className='absolute -top-24 right-5 md:-top-44 lg:top-0 lg:-left-10 text-3xl md:text-5xl font-UrbanistSemibold'>
               Piel Seca
-            </p>
-            <p className='mt-14 text-xl font-Urbanist'>
+            </h3>
+            <p className='lg:mt-14 text-base md:text-xl lg:text-xl font-Urbanist'>
               ★ La humectación diaria debe ser tu máxima prioridad si tienes la
               piel seca. No pueden faltar en tus productos: ácido hialurónico,
               ceramidas, aloe vera y avena coloidal. <br />
@@ -78,14 +94,14 @@ const Tips = () => {
               exposición solar y limpia tu rostro con agua templada.
             </p>
           </div>
-        </div>
+        </article>
 
-        <div className='flex flex-row items-center text-left'>
+        <article className='flex flex-col-reverse lg:flex-row items-center gap-3 text-left'>
           <div className='relative px-5'>
-            <p className='absolute -right-10 text-5xl font-UrbanistSemibold z-20'>
+            <h3 className='absolute -top-24 md:-top-44 lg:top-0 lg:-right-10 text-3xl md:text-5xl font-UrbanistSemibold z-20'>
               Piel Madura
-            </p>
-            <p className='mt-14 text-xl font-Urbanist'>
+            </h3>
+            <p className='lg:mt-14 text-base md:text-xl lg:text-xl font-Urbanist'>
               ★ Además de la aplicación, la forma en la que aplicamos nuestros
               productos es muy importante. Masajea tu piel gentilmente al usar
               tus productos para mejorar la absorción de nutrientes. <br />
@@ -98,24 +114,24 @@ const Tips = () => {
             </p>
           </div>
           <img
-            className='border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
+            className='w-1/2 sm:w-[45%] md:w-1/2 lg:w-auto pt-0 ml-auto lg:mx-5 md:pt-2 lg:pt-10 border-[8px] md:border-[14px] lg:border-[18px] rounded-2xl aspect-auto bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
             src='/assets/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1 (2).png'
             alt='belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo'
           />
-        </div>
+        </article>
 
-        <div className='flex flex-row items-center text-left'>
+        <article className='flex flex-col lg:flex-row items-center gap-3 text-left'>
           <img
-            className='border-[18px] rounded-2xl aspect-auto pt-10 mx-5 bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
+            className='w-1/2 sm:w-[45%] md:w-1/2 lg:w-auto pt-0 mr-auto lg:mx-5 md:pt-2 lg:pt-10 border-[8px] md:border-[14px] lg:border-[18px] rounded-2xl aspect-auto bg-gradient-to-t from-palette-ChampagnePink/60  border-white hover:brightness-105 transition-all'
             src='/assets/imgpage/belleza-cuidado-piel-mujer-joven-perfecta-maquillaje-nude-natural-hombros-desnudos-mostrando-piel-facial-nutrida-brillante-sonriendo-complacida-mirandose-espejo-despues-ducha-fondo-estudio-blanco 1 (3).png'
             alt='belleza-cuidado-piel-mujer-joven-perfecta-mostrando-piel-facial-nutrida-brillante-sonriendo'
           />
 
           <div className='relative px-5'>
-            <p className='absolute -left-10 text-5xl font-UrbanistSemibold'>
+            <h3 className='absolute -top-24 right-5 md:-top-44 lg:top-0 lg:-left-10 text-3xl md:text-5xl font-UrbanistSemibold'>
               Piel Grasa
-            </p>
-            <p className='mt-14 text-xl font-Urbanist'>
+            </h3>
+            <p className='lg:mt-14 text-base md:text-xl lg:text-xl font-Urbanist'>
               ★ La limpieza diurna y nocturna son fundamentales para reducir el
               nivel de sebo en la piel. No las dejes pasar. <br />
               ★ Los serums y cremas hidratantes no pueden faltar en tu rutina
@@ -126,15 +142,15 @@ const Tips = () => {
               cerrados. <br />
             </p>
           </div>
-        </div>
+        </article>
       </div>
-      <div className='p-10 bg-palette-OrchidPink'>
+      <div className='p-4 sm:p-10 bg-palette-OrchidPink'>
         <div className='p-1 bg-gradient-to-b from-palette-OldRose to-palette-ChampagnePink'>
           <div className=' flex flex-col gap-2 p-5 py-10 bg-palette-OrchidPink  z-20'>
-            <p className='text-5xl font-UrbanistSemibold'>
+            <p className='text-3xl md:text-5xl font-UrbanistSemibold'>
               Para tener en cuenta
             </p>
-            <p className='text-xl font-Urbanist'>
+            <p className='lg:mt-8 text-base md:text-xl lg:text-xl font-Urbanist'>
               ★ Para obtener resultados con una nueva rutina, tienes que esperar
               1-3 meses. <br />
               ★ Desmaquillante antes de irte a dormir. <br />
@@ -146,58 +162,37 @@ const Tips = () => {
           </div>
         </div>
       </div>
-      <span className='w-full grow sticky font-Cremona text-6xl text-center py-10 bg-palette-OrchidPink text-slate-700'>
+      <span className='w-full grow sticky font-Cremona text-3xl lg:text-6xl text-center py-10 bg-palette-OrchidPink text-slate-700'>
         AFIRMACIONES
       </span>
-      <div className='bg-fixed bg-origin-padding bg-center bg-no-repeat bg-cover bg-sky-img'>
-        <div className='grid grid-cols-3 auto-rows-8 gap-3 px-20 my-5'>
-          <div className='flex items-center row-start-1 row-end-4 py-18 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className=' text-stone-800 font-Cremona text-lg text-left px-6'>
-              ESTOY ABIERTA Y RECEPTIVA A RECIBIR TODA MAGIA QUE HAY PARA MÍ
-            </p>
+      <div className=' bg-fixed bg-origin-padding bg-center bg-no-repeat bg-cover bg-sky-img'>
+        <div className='grid grid-col-1 lg:grid-cols-3 auto-rows-auto lg:auto-rows-8 gap-3 md:px-8 lg:px-14 xl:px-20 my-5'>
+          <div className='flex items-center lg:row-start-1 lg:row-end-4 py-6 lg:py-18 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={0} />
           </div>
-          <div className='flex items-center row-start-1 row-end-3 py-14 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className='text-stone-800 font-Cremona text-lg text-left px-6'>
-              ATRAIGO TODA LA RIQUEZA QUE NECESITO Y DESEO CON EL MISMO
-              ESFUERZO.
-            </p>
+          <div className='flex items-center lg:row-start-1 lg:row-end-3 py-6 lg:py-14 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={1} />
           </div>
-          <div className='flex items-center row-start-1 row-end-4 py-18 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className='text-stone-800 font-Cremona text-lg text-left px-6'>
-              ESTOY ALINEADA CON EL UNIVERSO Y TENGO EL PODER DE VIVIR LA VIDA
-              DE MIS SUEÑOS.
-            </p>
+          <div className='flex items-center lg:row-start-1 lg:row-end-4 py-6 lg:py-18 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={2} />
           </div>
-          <div className='flex items-center row-start-4 row-end-6 py-30 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className='text-stone-800 font-Cremona text-lg text-left px-6'>
-              SOY ABUNDANTE DE MENTE, CUERPO, ALMA Y BOLSILLO.
-            </p>
+          <div className='flex items-center lg:row-start-4 lg:row-end-6 py-6 lg:py-30 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={3} />
           </div>
-          <div className='flex items-center row-start-3 row-end-5 py-14 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className='text-stone-800 font-Cremona text-lg text-left px-6'>
-              TODO LO QUE INVIERTO EN MI, REGRESA 70 VECES 7.
-            </p>
+          <div className='flex items-center lg:row-start-3 lg:row-end-5 py-6 lg:py-14 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={4} />
           </div>
-          <div className='flex items-center row-start-4 row-end-6 py-30 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className='text-stone-800 font-Cremona text-lg text-left px-6'>
-              TODO LO QUE YO QUIERO SE MANIFIESTA.
-            </p>
+          <div className='flex items-center lg:row-start-4 lg:row-end-6 py-6 lg:py-30 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={5} />
           </div>
-          <div className='flex items-center row-start-6 row-end-auto py-14 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className='text-stone-800 font-Cremona text-lg text-left px-6'>
-              TODO LO QUE DESEO LLEGA A MI CON FLUIDEZ Y FACILIDAD.
-            </p>
+          <div className='flex items-center lg:row-start-6 lg:row-end-auto py-6 lg:py-14 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={6} />
           </div>
-          <div className='flex items-center row-start-5 row-end-7 py-24 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className='text-stone-800 font-Cremona text-lg text-left px-6'>
-              CREER EN TU TALENTO, EN TU POTENCIAL Y RESPETAR TU ESFUERZO ES DE
-              LAS MEJORES FORMAS DE AMARTE.
-            </p>
+          <div className='flex items-center lg:row-start-5 lg:row-end-7 py-6 lg:py-24 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={7} />
           </div>
-          <div className='flex items-center row-start-6 row-end-auto py-14 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
-            <p className='text-stone-800 font-Cremona text-lg text-left px-6'>
-              TODO EL UNIVERSO CONSPIRA A MI FAVOR AQUÍ Y AHORA.
-            </p>
+          <div className='flex items-center lg:row-start-6 lg:row-end-auto py-6 lg:py-14 mx-3 md:mx-0 bg-gradient-to-b from-palette-ChampagnePink/50 to-palette-OrchidPink/50 border-2 border-palette-OldRose rounded-sm'>
+            <PTips type={8} />
           </div>
         </div>
       </div>
