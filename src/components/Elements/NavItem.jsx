e54cebde895fdc79edc8df39b85cbdf464cbc9ec
Franco Saldaño <francosaldano1@gmail.com>
2023-04-12 21:40:44 -0300

    update navbar ahora la navbar tiene navitems components que cambian de svg cuando están activos

---
+++ src/components/Elements/NavItem.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router-dom'
+
+const NavItem = ({ link, svg, svgActive, isActive, onClick }) => {
+
+    return (
+        <>
+            <li className='mt-auto'>
+                <NavLink
+                    to={link}
+                    onClick={onClick}
+                    className={({ isActive }) => {
+                        return (
+                            "block py-1 border-b-4 transition-all" +
+                            (isActive
+                                ? " border-palette-Auburn"
+                                : " border-transparent")
+                        )
+                    }}
+                >
+                    {isActive ? svgActive : svg}
+                </NavLink>
+            </li>
+        </>
+    )
+}
+
+export default NavItem
