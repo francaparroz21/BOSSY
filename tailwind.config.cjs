456862e0d21034e7e98b24e898422b3693f71066
Franco Saldaño <francosaldano1@gmail.com>
2023-04-19 00:05:19 -0300

    flowbite config deleted and update classes to grid class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,37 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './node_modules/flowbite/**/*.js',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        Cremona: ['Cremona', 'sans-serif'],
-        Urbanist: ['Urbanist', 'sans-serif'],
-        UrbanistMedium: ['UrbanistMedium', 'sans-serif'],
-        UrbanistSemibold: ['UrbanistSemibold', 'sans-serif'],
-        UrbanistBold: ['UrbanistBold', 'sans-serif']
+        Cremona: ["Cremona", "sans-serif"],
+        Urbanist: ["Urbanist", "sans-serif"],
+        UrbanistMedium: ["UrbanistMedium", "sans-serif"],
+        UrbanistSemibold: ["UrbanistSemibold", "sans-serif"],
+        UrbanistBold: ["UrbanistBold", "sans-serif"],
       },
       colors: {
         palette: {
-          Auburn: '#AA302B',
-          SoftAuburn: '#C25A5C',
-          Peach: '#F8B791',
-          SoftPeach: '#F8B8AF',
-          OldRose: '#AE7886',
-          OrchidPink: '#DEB2BF',
-          ChampagnePink: '#F1DDD6'
-        }
+          Auburn: "#AA302B",
+          SoftAuburn: "#C25A5C",
+          Peach: "#F8B791",
+          SoftPeach: "#F8B8AF",
+          OldRose: "#AE7886",
+          OrchidPink: "#DEB2BF",
+          ChampagnePink: "#F1DDD6",
+        },
       },
       boxShadow: {
-        't-xl': '0px -2px 22px 0px rgba(0,0,0,0.3)'
+        "t-xl": "0px -2px 22px 0px rgba(0,0,0,0.3)",
       },
       gridTemplateColumns: {
-        special: 'repeat(auto-fit, minmax(180px, 1fr))'
-      }
-    }
+        special: "repeat(auto-fit, minmax(280px, 1fr))",
+      },
+    },
   },
-  plugins: [require('./node_modules/flowbite/plugin')]
 }
