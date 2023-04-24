b18cbf2020d7a97a1587740521bdb2648a18718e
Franco Saldaño <francosaldano1@gmail.com>
2023-04-24 16:29:21 -0300

    useEffect to charge data

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,11 +3,20 @@ import { services } from "../assets/courses/data.json"
 import ItemDetail from "./ItemDetail"
 import Headers from "./Elements/Headers"
 import Card from "./Elements/Card"
+import { useEffect, useState } from 'react'
 
 const Services = () => {
   const { servicioId } = useParams()
-  const FIRSTSERVICECLASS = services.FYC
-  const SECONDSERVICECLASS = services.CPYM
+
+  const [firstServices, setFirstServices] = useState({})
+  const [secondServices, setSecondServices] = useState({})
+
+  useEffect(() => {
+    const FIRSTSERVICECLASS = services.FYC
+    const SECONDSERVICECLASS = services.CPYM
+    setFirstServices(FIRSTSERVICECLASS)
+    setSecondServices(SECONDSERVICECLASS)
+  }, [])
 
   return (
     <div className='w-full flex flex-col'>
@@ -19,7 +28,7 @@ const Services = () => {
         <>
           <Headers title={" Facial y Corporal"}></Headers>
           <div className='w-full grid justify-items-center grid-cols-special xl:grid-cols-4 grid-auto-row gap-3 md:gap-6 lg:gap-10 p-3 md:p-5 bg-palette-ChampagnePink/60 '>
-            {FIRSTSERVICECLASS.map((service) => (
+            {firstServices.map((service) => (
               <a href={`/servicios/${service.title}`} className='w-fit' key={service.id}>
                 <Card
                   img={service.img[0]}
@@ -31,7 +40,7 @@ const Services = () => {
           </div>
           <Headers title={"Cejas - Pestañas y Maquillaje"}></Headers>
           <div className='w-full grid justify-items-center grid-cols-special xl:grid-cols-4 grid-auto-row gap-3 md:gap-6 lg:gap-10 p-3 md:p-5 bg-palette-ChampagnePink/60 '>
-            {SECONDSERVICECLASS.map((service) => (
+            {secondServices.map((service) => (
               <a href={`/servicios/${service.title}`} className='w-fit' key={service.id}>
                 <Card
                   img={service.img[0]}
